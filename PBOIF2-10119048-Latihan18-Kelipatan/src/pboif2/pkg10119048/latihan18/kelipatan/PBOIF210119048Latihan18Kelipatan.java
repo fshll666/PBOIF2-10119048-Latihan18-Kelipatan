2eecf5637ded867cdc6cab20e5d1e3ec432cebbb
Mochammad Faishal <72900665+fshll666@users.noreply.github.com>
2020-10-29 00:03:26 +0700

    Add files via upload

---
+++ PBOIF2-10119048-Latihan18-Kelipatan/src/pboif2/pkg10119048/latihan18/kelipatan/PBOIF210119048Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119048.latihan18.kelipatan;
+
+/**
+ *
+ * @author SystemOs
+ * Nama      : Mochammad Faishal
+ * NIM       : 10119048
+ * Kelas     : IF2
+ * Deskripsi : Kelipatan
+ */
+public class PBOIF210119048Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (double i = 3.5; i <= 35; i+=3.5){
+               System.out.println(i);
+    }
+    System.out.println("==============================");
+    System.out.println("Developed by : Mochammad Faishal");
+}
+}
